87cf0d743981877fabf99f0942e1d6492d3f675d
Fearon <jiangfeifan@ijovo.com>
2020-04-11 10:35:09 +0800

    删除冗余代码

--- src/main/java/org/fearon/App.java
+++ src/main/java/org/fearon/App.java
@@ -1,13 +1,5 @@
 package org.fearon;
 
-import cn.afterturn.easypoi.excel.entity.params.ExcelExportEntity;
-import utils.Logger;
-import utils.poi.*;
-
-import java.io.*;
-import java.nio.charset.StandardCharsets;
-import java.util.*;
-
 /**
  * Hello world!
  */
